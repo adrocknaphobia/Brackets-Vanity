96804d120cf72cb81caeaff7ec687c59c9555eff
Garth Braithwaite <garthdb@gmail.com>
2012-04-24 01:10:00 -0700

    Finished up slides (just need to extend the selection indicator) and tweak animation

--- hero/js/app.coffee
+++ hero/js/app.coffee
@@ -1,5 +1,15 @@
 $ ->
 	$('#hero li a').click ->
+		index = $('#hero li').index($(this).parent())
 		$('#hero li').removeClass('selected')
 		$(this).parent().addClass('selected')
+
+		$('#hero .slide').removeClass('before after selected')
+
+
+		before = $('#hero .slide').slice(0, index)
+		before.addClass('before')
+		$($('#hero li')[index]).addClass('selected')
+		after = $('#hero .slide').slice(index + 1)
+		after.addClass('after')
 		return false
--- hero/js/app.js
+++ hero/js/app.js
@@ -3,8 +3,16 @@
 
   $(function() {
     return $('#hero li a').click(function() {
+      var after, before, index;
+      index = $('#hero li').index($(this).parent());
       $('#hero li').removeClass('selected');
       $(this).parent().addClass('selected');
+      $('#hero .slide').removeClass('before after selected');
+      before = $('#hero .slide').slice(0, index);
+      before.addClass('before');
+      $($('#hero li')[index]).addClass('selected');
+      after = $('#hero .slide').slice(index + 1);
+      after.addClass('after');
       return false;
     });
   });
